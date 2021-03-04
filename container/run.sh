3e0d87b77c2546fe9cf1a56861a28a2d713dda72
alireza <sdamoosavi@gmail.com>
2021-03-04 03:38:44 +0330

    docker image issue fixed

--- container/run.sh
+++ container/run.sh
@@ -3,10 +3,11 @@
 set -o errexit
 set -o nounset
 
+# uncomment below when you see any issue with db...
+# mlflow db upgrade $DB_URI
+
 mlflow server \
-    --backend-store-uri $DB_URI \
     --host $SERVER_HOST \
     --port $SERVER_PORT \
-    --default-artifact-root $ARTIFACT_STORE
-
-# mlflow ui --port $SERVER_PORT
+    --backend-store-uri $DB_URI \
+    --default-artifact-root $MLFLOW_ARTIFACT_STORE
